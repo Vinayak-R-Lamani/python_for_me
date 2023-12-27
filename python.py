6df9293a8a56034b8f54cb2b03ec548d0185b367
project <rajulamani9888@gmail.com>
2023-12-27 22:45:31 +0530

    first commit

--- python.py
+++ python.py
@@ -9,3 +9,14 @@ nums.append(3)
 
 for i in nums:
     print(i)
+
+# set function use remove the duplicate in arrya 
+
+myset = set(['geeks','for','geeks'])
+print(myset)
+
+
+x = int(input('enter the number :'))
+y = int(input('enter the number :'))
+
+print(f'the sum is {x+y}')
